7929889633cd18ea82d42ce82661fac5e89a6a91
Sosena Hagos <taklayso@gmail.com>
2025-09-14 09:25:53 -0700

    header and footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+import Home from './pages/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    
+      <div className='APP'>
+        <Home/>
         {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -25,12 +25,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
           
         </p> */}
-        <h1>hello</h1>
+        
       </div>
-      <p className="read-the-docs">
+      /* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */
+    
   )
 }
 
